310fc098e2f65a75643560eeffd741833d35632e
Krittipoom <krittipoom@outlook.com>
2023-09-19 14:51:09 +0700

    add color field & add smartphone model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Brand, Computer
+from .models import Brand, Computer, Smartphone
 
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(Computer)
+admin.site.register(Smartphone)
--- main/models.py
+++ main/models.py
@@ -15,10 +15,11 @@ class Computer(models.Model):
 	title = models.CharField(max_length=100, blank=False)
 	brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
 	image = models.ImageField(upload_to="model_img/computers/", blank=True, null=True)
+	color = models.CharField(max_length=100, default="Black", blank=False)
 
 	is_instock = models.BooleanField(default=True)
 	instocks = models.IntegerField(default=100, blank=False)
-	is_onsale = models.BooleanField(default=True)
+	is_onsale = models.BooleanField(default=True, blank=False)
 	og_price = models.IntegerField(default=22000, blank=False)
 	price = models.IntegerField(default=19900, blank=False)
 	stars = models.FloatField(default=5.0, blank=False)
@@ -27,6 +28,7 @@ class Computer(models.Model):
 	is_laptop = models.BooleanField(default=True)
 	is_gamingtype = models.BooleanField(default=False)
 	has_monitor = models.BooleanField(default=True)
+
 	CPU_BRANDS = models.TextChoices("CPU_BRANDS", "Reda Bluei")
 	RAM_CHOICES = models.TextChoices("RAM_CHOICES", "4Gb 8Gb 16Gb 32Gb 64Gb")
 	GPU_BRANDS = models.TextChoices("GPU_BRANDS", "Reda Bluei Greenn")
@@ -44,7 +46,7 @@ class Computer(models.Model):
 	ram = models.CharField(default="16Gb", choices=RAM_CHOICES.choices, max_length=10, blank=False)
 	gpu = models.CharField(max_length=100)
 	gpu_brand = models.CharField(default="Greenn", choices=GPU_BRANDS.choices, max_length=10, blank=False)
-	storage = models.CharField(max_length=10)
+	storage = models.CharField(default="512Gb", choices=STORAGE_CHOICES.choices, max_length=10, blank=False)
 	storage_type = models.CharField(default="Ssd", choices=STORAGE_TYPE.choices, max_length=10, blank=False)
 	size = models.CharField(default='15.6"', choices=SIZE_CHOICES.choices, max_length=10, blank=True)
 	case = models.CharField(choices=CASE_CHOICES.choices, max_length=10, blank=True)
@@ -53,6 +55,45 @@ class Computer(models.Model):
 	display = models.CharField(default="Ips",choices=DISPLAY_CHOICES.choices, max_length=10, blank=True)
 	weight = models.FloatField(default=1.70, blank=False)
 	os = models.CharField(default="Windows11", choices=OS_CHOICES.choices, max_length=50)
+
+	def __str__(self):
+		return self.title
+
+class Smartphone(models.Model):
+
+	title = models.CharField(max_length=100, blank=False)
+	brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to="model_img/smartphones/", blank=True, null=True)
+	color = models.CharField(max_length=100, default="White", blank=False)
+
+	is_instock = models.BooleanField(default=True)
+	instocks = models.IntegerField(default=100, blank=False)
+	is_onsale = models.BooleanField(default=True, blank=False)
+	og_price = models.IntegerField(default=8500, blank=False)
+	price = models.IntegerField(default=7900, blank=False)
+	stars = models.FloatField(default=5.0, blank=False)
+	is_recommend = models.BooleanField(default=False)
+
+	is_tablet = models.BooleanField(default=False)
+	is_flagship = models.BooleanField(default=False)
+
+	PROCESSOR_BRANDS = models.TextChoices("PROCESSOR_BRANDS", "Snapd Easy")
+	RAM_CHOICES = models.TextChoices("RAM_CHOICES", "3Gb 4Gb 8Gb 6Gb 12Gb 16Gb")
+	STORAGE_CHOICES = models.TextChoices("STORAGE_CHOICES", "32Gb 64Gb 128Gb 256Gb 512Gb")
+	SIZE_CHOICES = models.TextChoices("SIZE_CHOICES", '5.8" 6.0" 6.2" 6.4" 6.6" 6.8" 9.7" 10.1" 12"')
+	RESOLUTION_CHOICES = models.TextChoices("RESOLUTION_CHOICES", "1080P 1440P 4K")
+	REFRESHRATE_CHOICES = models.TextChoices("REFRESHRATE_CHOICES", "60Hz 75Hz 90Hz 120Hz 144Hz 240Hz")
+	DISPLAY_CHOICES = models.TextChoices("DISPLAY_CHOICES", "Ips Va Oled")
+
+	processor = models.CharField(max_length=100)
+	processor_brand = models.CharField(default="Snapd", choices=PROCESSOR_BRANDS.choices, max_length=10, blank=False)
+	ram = models.CharField(default="6Gb", choices=RAM_CHOICES.choices, max_length=10, blank=False)
+	storage = models.CharField(default="128Gb", choices=STORAGE_CHOICES.choices, max_length=10, blank=False)
+	size = models.CharField(default='6.2"', choices=SIZE_CHOICES.choices, max_length=10, blank=True)
+	resolution = models.CharField(default="1440P", choices=RESOLUTION_CHOICES.choices, max_length=10, blank=True)
+	refreshrate = models.CharField(default="90Hz", choices=REFRESHRATE_CHOICES.choices, max_length=10, blank=True)
+	display = models.CharField(default="Ips",choices=DISPLAY_CHOICES.choices, max_length=10, blank=True)
+	weight = models.FloatField(default=0.14, blank=False)
 
 	def __str__(self):
 		return self.title
