7c11b243e96dfe2970a2c0d87de6891319d854c4
Krittipoom <krittipoom@outlook.com>
2023-09-28 15:23:56 +0700

    register order_manager model in admin

--- code/main/admin.py
+++ code/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from products.models import *
+from order_manager.models import *
 
 # Register your models here.
 admin.site.register(Brand)
@@ -7,3 +8,7 @@ admin.site.register(Computer)
 admin.site.register(Smartphone)
 admin.site.register(Headphone)
 admin.site.register(Cloth)
+
+admin.site.register(ProductItem)
+admin.site.register(OrderProductItem)
+admin.site.register(Order)
