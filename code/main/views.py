42f2023f7f413d919a183a0ee7f6a39942ab01dc
Krittipoom <krittipoom@outlook.com>
2023-10-02 09:58:18 +0700

    organize first request function to new directory

--- code/main/views.py
+++ code/main/views.py
@@ -4,192 +4,102 @@ from django.contrib import messages
 from django.utils import timezone
 from products.models import *
 from order_manager.models import *
-from math import floor
+
+from .get_first_request.get_all_computer_data import GetAllComputerData
+from .get_first_request.get_all_smartphone_data import GetAllSmartphoneData
 
 # Create your views here.
 def home(request):
 
-    if 'computer_data' not in request.session:
-        filtered_computer = Computer.objects.all()
+	if 'computer_data' not in request.session:
+		request.session['computer_data'] = GetAllComputerData()
 
-        computer_title = [x[0] for x in filtered_computer.values_list('title')]
-        computer_link = ['/computer/product/'+str(x[0]) for x in filtered_computer.values_list('id')]
-        computer_onsale = [x[0] for x in filtered_computer.values_list('is_on_sale')]
-        computer_og_price = [x[0] for x in filtered_computer.values_list('og_price')]
-        computer_price = [x[0] for x in filtered_computer.values_list('price')]
-        computer_is_in_stock = [x[0] for x in filtered_computer.values_list('is_in_stock')]
-        computer_in_stocks = [x[0] for x in filtered_computer.values_list('in_stocks')]
-        computer_is_recommend  = [x[0] for x in filtered_computer.values_list('is_recommend')]
-        computer_img = filtered_computer.values_list('image')
-        computer_img = [x[0].replace(' ','%20') for x in computer_img]
-        computer_price = [str(x[0]) for x in filtered_computer.values_list('price')]
-        computer_star = [str(x[0]) for x in filtered_computer.values_list('stars')]
+	if 'smartphone_data' not in request.session:
+		request.session['smartphone_data'] = GetAllSmartphoneData()
 
-        computer_data = []
-        for i in range(len(computer_title)):
-
-            if computer_onsale[i] == False:
-                computer_og_price[i] = ''
-            fullstar = "★" * floor(float(computer_star[i]))
-
-            if computer_onsale[i] == True:
-                computer_onsale[i] = 'Sale'
-            else:
-                computer_onsale[i] = ''
-
-            if computer_is_in_stock[i] == False:
-                computer_is_in_stock[i] = 'Out of Stock'
-            else:
-                computer_is_in_stock[i] = 'In Stocks'
-
-            if computer_is_recommend[i] == True:
-                computer_is_recommend[i] = 'Recommend'
-            else:
-                computer_is_recommend[i] = ''
-
-            if len(computer_title[i] + 'Recommend') > 37:
-                computer_title[i] = computer_title[i][0:30] + '...'
-
-
-            computer_data.append({'title':computer_title[i], 'onsale':computer_onsale[i], 
-                'ogprice':computer_og_price[i], 'price':computer_price[i],'im':computer_img[i], 
-                'instock':computer_is_in_stock[i], 'available': computer_in_stocks[i], 'link': computer_link[i],
-                'recommend':computer_is_recommend[i], 'star':fullstar, 'star_num':' ('+computer_star[i]+')'})
-
-        request.session['computer_data'] = computer_data
-
-    if 'smartphone_data' not in request.session:     
-        filtered_smartphone =  Smartphone.objects.all()
-
-        smartphone_title = [x[0] for x in filtered_smartphone.values_list('title')]
-        smartphone_link = ['/smartphone/product/'+str(x[0]) for x in filtered_smartphone.values_list('id')]
-        smartphone_onsale = [x[0] for x in filtered_smartphone.values_list('is_on_sale')]
-        smartphone_og_price = [x[0] for x in filtered_smartphone.values_list('og_price')]
-        smartphone_price = [x[0] for x in filtered_smartphone.values_list('price')]
-        smartphone_is_in_stock = [x[0] for x in filtered_smartphone.values_list('is_in_stock')]
-        smartphone_in_stocks = [x[0] for x in filtered_smartphone.values_list('in_stocks')]
-        smartphone_is_recommend  = [x[0] for x in filtered_smartphone.values_list('is_recommend')]
-        smartphone_img = filtered_smartphone.values_list('image')
-        smartphone_img = [x[0].replace(' ','%20') for x in smartphone_img]
-        smartphone_price = [str(x[0]) for x in filtered_smartphone.values_list('price')]
-        smartphone_star = [str(x[0]) for x in filtered_smartphone.values_list('stars')]
-
-        smartphone_data = []
-        for i in range(len(smartphone_title)):
-
-            if smartphone_onsale[i] == False:
-                smartphone_og_price[i] = ''
-            fullstar = "★" * floor(float(smartphone_star[i]))
-
-            if smartphone_onsale[i] == True:
-                smartphone_onsale[i] = 'Sale'
-            else:
-                smartphone_onsale[i] = ''
-
-            if smartphone_is_in_stock[i] == False:
-                smartphone_is_in_stock[i] = 'Out of Stock'
-            else:
-                smartphone_is_in_stock[i] = 'In Stocks'
-
-            if smartphone_is_recommend[i] == True:
-                smartphone_is_recommend[i] = 'Recommend'
-            else:
-                smartphone_is_recommend[i] = ''
-
-            if len(smartphone_title[i] + 'Recommend') > 37:
-                smartphone_title[i] = smartphone_title[i][0:30] + '...'
-
-
-            smartphone_data.append({'title':smartphone_title[i], 'onsale':smartphone_onsale[i], 
-                'ogprice':smartphone_og_price[i], 'price':smartphone_price[i],'im':smartphone_img[i], 
-                'instock':smartphone_is_in_stock[i], 'available': smartphone_in_stocks[i], 'link': smartphone_link[i],
-                'recommend':smartphone_is_recommend[i], 'star':fullstar, 'star_num':' ('+smartphone_star[i]+')'})
-
-        request.session['smartphone_data'] = smartphone_data
-
-    return render(request, "homepage.html")
+	return render(request, "homepage.html")
 
 def product_computer(request, id):
-    product = Computer.objects.get(id=id)
-    fullstar = "★" * floor(product.stars)
-    return render(request, "store/product/computer.html", {'data': product, 'fullstar': fullstar})
+	product = Computer.objects.get(id=id)
+	fullstar = "★" * floor(product.stars)
+	return render(request, "store/product/computer.html", {'data': product, 'fullstar': fullstar})
 
 def product_smartphone(request, id):
-    product = Smartphone.objects.get(id=id)
-    fullstar = "★" * floor(product.stars)
-    return render(request, "store/product/smartphone.html", {'data': product, 'fullstar': fullstar})
+	product = Smartphone.objects.get(id=id)
+	fullstar = "★" * floor(product.stars)
+	return render(request, "store/product/smartphone.html", {'data': product, 'fullstar': fullstar})
 
 @login_required
 def add_to_cart(request, title):
-    prod_item = get_object_or_404(ProductItem, title=title)    
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
-    
-    if order_qs.exists():
-        order = order_qs[0]
-        
-        if order.items.filter(prod_item=prod_item, ordered=False).exists():
-            order_item, created = order.items.get_or_create(prod_item=prod_item, ordered=False)
-            order_item.quantity += 1
-            order_item.save()
-            messages.info(request, "This item quality from your cart was updated.")
-            return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-        else:
-            order_item = OrderProductItem.objects.create(prod_item=prod_item, user=request.user, ordered=False)
-            order.items.add(order_item)
-            messages.info(request, "This item was added to your cart.")
-            return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-    else:
-        ordered_date = timezone.now()
-        order = Order.objects.create(user=request.user, ordered_date=ordered_date)
-        order_item = OrderProductItem.objects.create(prod_item=prod_item, user=request.user, ordered=False)
-        order.items.add(order_item)
-        messages.info(request, "This item was added to from your cart.")
-        return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+	prod_item = get_object_or_404(ProductItem, title=title)    
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
+	
+	if order_qs.exists():
+		order = order_qs[0]
+		
+		if order.items.filter(prod_item=prod_item, ordered=False).exists():
+			order_item, created = order.items.get_or_create(prod_item=prod_item, ordered=False)
+			order_item.quantity += 1
+			order_item.save()
+			messages.info(request, "This item quality from your cart was updated.")
+			return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+		else:
+			order_item = OrderProductItem.objects.create(prod_item=prod_item, user=request.user, ordered=False)
+			order.items.add(order_item)
+			messages.info(request, "This item was added to your cart.")
+			return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+	else:
+		ordered_date = timezone.now()
+		order = Order.objects.create(user=request.user, ordered_date=ordered_date)
+		order_item = OrderProductItem.objects.create(prod_item=prod_item, user=request.user, ordered=False)
+		order.items.add(order_item)
+		messages.info(request, "This item was added to from your cart.")
+		return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
 
 @login_required
 def remove_from_cart(request, title):
-    prod_item = get_object_or_404(ProductItem, title=title)
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
+	prod_item = get_object_or_404(ProductItem, title=title)
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
 
-    if order_qs.exists():
-        order = order_qs[0]
+	if order_qs.exists():
+		order = order_qs[0]
 
-        if order.items.filter(prod_item__title=prod_item.title).exists():
-            order_item = OrderProductItem.objects.filter(prod_item=prod_item, user=request.user, ordered=False)[0]
-            order.items.remove(order_item)
-            order_item.delete()
-            messages.info(request, "This item was removed from your cart.")
-            return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-        else:
-            messages.info(request, "This item was not in your cart")
-            return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-    else:
-        messages.info(request, "You do not have an active order")
-        return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+		if order.items.filter(prod_item__title=prod_item.title).exists():
+			order_item = OrderProductItem.objects.filter(prod_item=prod_item, user=request.user, ordered=False)[0]
+			order.items.remove(order_item)
+			order_item.delete()
+			messages.info(request, "This item was removed from your cart.")
+			return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+		else:
+			messages.info(request, "This item was not in your cart")
+			return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+	else:
+		messages.info(request, "You do not have an active order")
+		return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
 
 
 @login_required
 def remove_single_item_from_cart(request, title):
-    prod_item = get_object_or_404(ProductItem, title=title)
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
+	prod_item = get_object_or_404(ProductItem, title=title)
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
 
-    if order_qs.exists():
-        order = order_qs[0]
+	if order_qs.exists():
+		order = order_qs[0]
 
-        if order.items.filter(prod_item__title=prod_item.title).exists():
-            order_item = OrderProductItem.objects.filter(prod_item=prod_item, user=request.user, ordered=False)[0]
-            if order_item.quantity > 1:
-                order_item.quantity -= 1
-                order_item.save()
-                messages.info(request, "This item quantity was updated.")
-                return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-            else:
-                order.items.remove(order_item)
-                messages.info(request, "This item quantity was updated.")
-                return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-        else:
-            messages.info(request, "This item was not in your cart")
-            return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
-    else:
-        messages.info(request, "You do not have an active order")
-        return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+		if order.items.filter(prod_item__title=prod_item.title).exists():
+			order_item = OrderProductItem.objects.filter(prod_item=prod_item, user=request.user, ordered=False)[0]
+			if order_item.quantity > 1:
+				order_item.quantity -= 1
+				order_item.save()
+				messages.info(request, "This item quantity was updated.")
+				return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+			else:
+				order.items.remove(order_item)
+				messages.info(request, "This item quantity was updated.")
+				return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+		else:
+			messages.info(request, "This item was not in your cart")
+			return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
+	else:
+		messages.info(request, "You do not have an active order")
+		return redirect("view_product_computer", id=Computer.objects.get(title=title).id)
